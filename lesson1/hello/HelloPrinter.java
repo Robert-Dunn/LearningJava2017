2a5fea119899536b9fb214c8cdc35b8452bb5c7f
Robert Dunn <rdskifreak@gmail.com>
2017-04-28 16:37:56 -0600

    Changed hello file to have new line before mom.

--- lesson1/hello/HelloPrinter.java
+++ lesson1/hello/HelloPrinter.java
@@ -2,6 +2,6 @@ public class HelloPrinter
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World and Mom!");
+        System.out.println("Hello, World \nAnd Mom!");
     }
 }
