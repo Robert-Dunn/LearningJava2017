7bccefb88bea56a5d52c890161360518652dad9a
Robert Dunn <rdskifreak@gmail.com>
2017-04-28 15:06:45 -0600

    Tested the difference between print and println in text and numbers.

--- lesson1/textAndNumbers/Test.java
+++ lesson1/textAndNumbers/Test.java
@@ -5,7 +5,7 @@ public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println(  );
-        System.out.println(  );
+        System.out.print(3);
+        System.out.println( 4+5 );
     }
 }
